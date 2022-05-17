99a69818d838f038f24bccd85a16fe179c167bc1
Angel Yan <angelyan96@gmail.com>
2022-05-17 12:48:09 -0300

    Correr prettier

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,13 +1,13 @@
-import cors from 'cors'
-const express = require('express')
-const app = express()
-app.use(cors())
-const port = 3001
+import cors from 'cors';
+const express = require('express');
+const app = express();
+app.use(cors());
+const port = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 const axios = require('axios');
 function App() {
-    const [state, setState] = useState('sarlonga');
+  const [state, setState] = useState('sarlonga');
 
-    useEffect(() => {
-        async function saraza() {
-            const response = await axios.get('http://localhost:3001');
-            debugger;
-            setState(response.data);
-        }
-        saraza();
-    }, []);
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    {state}
-                </a>
-            </header>
-        </div>
-    );
+  useEffect(() => {
+    async function saraza() {
+      const response = await axios.get('http://localhost:3001');
+      debugger;
+      setState(response.data);
+    }
+    saraza();
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          {state}
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
