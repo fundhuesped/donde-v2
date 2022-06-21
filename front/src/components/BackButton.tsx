e6558a30625766bf91dffbfdf991448db7655b6b
Emmanuel Chang <emmanuel.chang@10pines.com>
2022-06-21 17:41:33 -0300

    wip

---
+++ front/src/components/BackButton.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+import { ChevronLeftIcon } from '@heroicons/react/solid';
+
+export const BackButton = () => {
+  let navigate = useNavigate();
+  const handleBackButtonClicked = () => navigate(-1);
+  return (
+    <button onClick={handleBackButtonClicked}>
+      <ChevronLeftIcon className="h-8 w-8 ml-2 text-dark-gray" />
+    </button>
+  );
+};
--- front/src/pages/Details.tsx
+++ front/src/pages/Details.tsx
@@ -1,18 +1,11 @@
-import { ChevronLeftIcon } from '@heroicons/react/solid';
-import { Link } from 'react-router-dom';
 import MainContainer from '../components/MainContainer';
+import { BackButton } from '../components/BackButton';
 
 export const Details = () => {
   return (
     <>
-      <div>
-        <Link to="/">
-          <ChevronLeftIcon className="h-8 w-8 ml-2 text-dark-gray" />
-        </Link>
-      </div>
-      <MainContainer>
-
-      </MainContainer>
+      <BackButton />
+      <MainContainer></MainContainer>
     </>
   );
 };
