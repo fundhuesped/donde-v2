e9a23514d4f1bae3e91be402574f571b54e33989
Emmanuel Chang <emmanuel.chang@10pines.com>
2022-06-21 18:30:14 -0300

    fix styles, use card components

--- front/src/pages/Details.tsx
+++ front/src/pages/Details.tsx
@@ -1,25 +1,23 @@
 import MainContainer from '../components/MainContainer';
 import { BackButton } from '../components/BackButton';
-import {Card, CardList, CardListItem, CardParagraph, CardSubHeader} from "../components/Card";
-import {ClockIcon, LocationMarkerIcon} from "@heroicons/react/outline";
-import classNames from "classnames";
-import {Pill} from "../components/Pill";
-import React from "react";
+import { Card, CardHeader, CardList, CardListItem, CardParagraph, CardSubHeader } from '../components/Card';
+import { ClockIcon, LocationMarkerIcon } from '@heroicons/react/outline';
+import { Pill } from '../components/Pill';
+import React from 'react';
 
 import { ReactComponent as TestDeVIH } from '../assets/images/TestDeVIH.svg';
 import { ReactComponent as Condones } from '../assets/images/Condones.svg';
-
 
 export const Details = () => {
   return (
     <>
       <BackButton />
       <MainContainer>
-        <div className={"mt-10"}>
-          <div className={"text-lg font-title font-bold"}>Nombre del establecimiento</div>
-          <div className={"text-sm"}>Tipo de establecimiento</div>
+        <div className={'mt-10 ml-4'}>
+          <CardHeader className={'font-title text-lg'}>Nombre del establecimiento</CardHeader>
+          <CardParagraph>Tipo de establecimiento</CardParagraph>
         </div>
-        <Card className={"mt-4"}>
+        <Card className={'mt-4'}>
           <CardList>
             <CardListItem icon={<LocationMarkerIcon className={'text-primary'} />}>
               Calle 1234, CABA <span className={'text-xs text-medium-gray'}>- A 400 metros</span>
@@ -34,9 +32,12 @@ export const Details = () => {
           </CardList>
 
           <CardSubHeader>Otros datos</CardSubHeader>
-          <CardParagraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardParagraph>
+          <CardParagraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua.
+          </CardParagraph>
 
-          <footer className={classNames('mt-4')}>
+          <footer className={'mt-4'}>
             <Pill>Cargado por Fundación Huesped</Pill>
           </footer>
         </Card>
