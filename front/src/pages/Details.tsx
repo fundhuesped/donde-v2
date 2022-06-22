10c2375bce463287cab87f41c5e0f26ea2b2971e
Emmanuel Chang <emmanuel.chang@10pines.com>
2022-06-22 16:02:10 -0300

    wip

--- front/src/pages/Details.tsx
+++ front/src/pages/Details.tsx
@@ -1,45 +1,119 @@
 import MainContainer from '../components/MainContainer';
 import { BackButton } from '../components/BackButton';
+import { Icon } from '../components/Icon';
+import { Pill } from '../components/Pill';
 import { Card, CardHeader, CardList, CardListItem, CardParagraph, CardSubHeader } from '../components/Card';
 import { ClockIcon, LocationMarkerIcon, GlobeAltIcon, PhoneIcon, ShareIcon } from '@heroicons/react/outline';
-import { Pill } from '../components/Pill';
+import { ReactComponent as WhatsAppLogo } from '../assets/images/WhatsAppLogo.svg';
+
 import React from 'react';
-
-import { ReactComponent as TestDeVIH } from '../assets/images/TestDeVIH.svg';
 import { ReactComponent as Condones } from '../assets/images/Condones.svg';
-import { Icon } from '../components/Icon';
+import { ReactComponent as TestDeVIH } from '../assets/images/TestDeVIH.svg';
 
-const aaa = {
-    key: 1,
-    lat: -34.602086,
-    lng: -58.384543,
-    address: 'Tribunales',
-    schedule: 'Lunes a Viernes de 12 a 18',
+
+interface WebSiteButtonProps {
+  website: string;
+}
+
+const WebSiteButton = React.memo<WebSiteButtonProps>((props) => {
+  const { website } = props;
+  return (
+    <a href={website} role="button">
+      <Icon type={'secondary'} circle={true} icon={<GlobeAltIcon className={'text-primary'} />} />
+      <div className={'pt-2 text-center text-xs text-primary'}>{'Sitio Web'}</div>
+    </a>
+  );
+});
+
+interface PhoneButtonProps {
+  phone: string;
+}
+
+const PhoneButton = React.memo<PhoneButtonProps>((props) => {
+  const { phone } = props;
+  return (
+    <a href={'tel:' + phone} role="button">
+      <Icon type={'secondary'} circle={true} icon={<PhoneIcon className={'text-primary'} />} />
+      <div className={'pt-2 text-center text-xs text-primary'}>{'Llamar'}</div>
+    </a>
+  );
+});
+const handleClick = async () => {
+  const shareData = {
+    title: 'MDN',
+    text: 'Learn web development on MDN!',
+    url: 'https://developer.mozilla.org',
   };
 
+  if (navigator['share']) {
+    try {
+      await navigator['share'](shareData);
+      console.log('hola');
+    } catch (err) {
+      console.log('Error: ' + err);
+    }
+  }
+};
+
+const ShareButton = () => {
+  return (
+    <button onClick={handleClick}>
+      <Icon type={'secondary'} circle={true} icon={<ShareIcon className={'text-primary'} />} />
+      <div className={'pt-2 text-center text-xs text-primary'}>{'Compartir'}</div>
+    </button>
+  );
+};
+
+interface WhatsAppButtonProps {
+  phone: string;
+}
+
+const WhatsAppButton = React.memo<WhatsAppButtonProps>((props) => {
+  const { phone } = props;
+  return (
+    // https://faq.whatsapp.com/general/chats/how-to-use-click-to-chat
+    <a href={'https://wa.me/' + phone} role="button">
+      <Icon type={'secondary'} circle={true} icon={<WhatsAppLogo className={'text-primary'} />} />
+      <div className={'pt-2 text-center text-xs text-primary'}>{'WhatsApp'}</div>
+    </a>
+  );
+});
+
 export const Details = () => {
+  const name = 'Nombre del establecimiento';
+  const type = 'Tipo de establecimiento';
+  const address = 'Calle 1234, CABA';
+  const addressNotes = 'A 400 metros';
+  const businessHours = 'Lunes a Sábados de 9 a 20 hs';
+  const services = [
+    { id: 'test-hiv', name: 'Test de HIV', icon: <TestDeVIH /> },
+    { id: 'preservativos', name: 'Preservativos', icon: <Condones /> },
+  ];
   return (
     <>
       <BackButton />
       <MainContainer>
         <div className={'mt-10 ml-4'}>
-          <CardHeader className={'font-title text-lg'}>Nombre del establecimiento</CardHeader>
-          <CardParagraph>Tipo de establecimiento</CardParagraph>
+          <CardHeader className={'font-title text-lg'}>{name}</CardHeader>
+          <CardParagraph>{type}</CardParagraph>
         </div>
-        <Card className={'mt-4'}>
+        <Card className={'my-4'}>
           <CardList>
             <CardListItem icon={<LocationMarkerIcon className={'text-primary'} />}>
-              Calle 1234, CABA <span className={'text-xs text-medium-gray'}>- A 400 metros</span>
+              {address} <span className={'text-xs text-medium-gray'}>- {addressNotes}</span>
             </CardListItem>
-            <CardListItem icon={<ClockIcon className={'text-primary'} />}>
-              Lunes a Sábados de 9 a 20 hs
-            </CardListItem>
+            <CardListItem icon={<ClockIcon className={'text-primary'} />}>{businessHours}</CardListItem>
           </CardList>
 
           <CardSubHeader>Servicios disponibles</CardSubHeader>
           <CardList>
-            <CardListItem icon={<TestDeVIH />}>Test de HIV</CardListItem>
-            <CardListItem icon={<Condones />}>Preservativos</CardListItem>
+            {services.map((service) => {
+              return (
+                <CardListItem key={service.id} icon={service.icon}>
+                  {service.name}
+                </CardListItem>
+              );
+            })}
           </CardList>
 
           <CardSubHeader>Otros datos</CardSubHeader>
@@ -53,17 +127,12 @@ export const Details = () => {
           </footer>
         </Card>
 
-        <div className={'flex-row'}>
-          <div>
-            <Icon type={'secondary'} circle={true} icon={<GlobeAltIcon className={'text-primary'} />} />
-            <span className={'pt-2 text-center text-xs text-primary'}>{'Sitio Web'}</span>
-          </div>
-          <div>
-            <Icon type={'secondary'} circle={true} icon={<PhoneIcon className={'text-primary'} />} />
-            <span className={'pt-2 text-center text-xs text-primary'}>{'Llamar'}</span>
-          </div>
+        <div className={'flex justify-center space-x-7 my-9'}>
+          <WebSiteButton website={'//10pines.com'} />
+          <PhoneButton phone={'541131213390'} />
+          <ShareButton />
+          <WhatsAppButton phone={'541131213390'} />
         </div>
-
       </MainContainer>
     </>
   );
