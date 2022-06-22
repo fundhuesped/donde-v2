b57017957d2a46093cfba89101476af8ca657251
Emmanuel Chang <emmanuel.chang@10pines.com>
2022-06-22 11:25:52 -0300

    wip

--- front/src/pages/Details.tsx
+++ front/src/pages/Details.tsx
@@ -1,12 +1,21 @@
 import MainContainer from '../components/MainContainer';
 import { BackButton } from '../components/BackButton';
 import { Card, CardHeader, CardList, CardListItem, CardParagraph, CardSubHeader } from '../components/Card';
-import { ClockIcon, LocationMarkerIcon } from '@heroicons/react/outline';
+import { ClockIcon, LocationMarkerIcon, GlobeAltIcon, PhoneIcon, ShareIcon } from '@heroicons/react/outline';
 import { Pill } from '../components/Pill';
 import React from 'react';
 
 import { ReactComponent as TestDeVIH } from '../assets/images/TestDeVIH.svg';
 import { ReactComponent as Condones } from '../assets/images/Condones.svg';
+import { Icon } from '../components/Icon';
+
+const aaa = {
+    key: 1,
+    lat: -34.602086,
+    lng: -58.384543,
+    address: 'Tribunales',
+    schedule: 'Lunes a Viernes de 12 a 18',
+  };
 
 export const Details = () => {
   return (
@@ -22,7 +31,9 @@ export const Details = () => {
             <CardListItem icon={<LocationMarkerIcon className={'text-primary'} />}>
               Calle 1234, CABA <span className={'text-xs text-medium-gray'}>- A 400 metros</span>
             </CardListItem>
-            <CardListItem icon={<ClockIcon className={'text-primary'} />}>Lunes a Sábados de 9 a 20 hs</CardListItem>
+            <CardListItem icon={<ClockIcon className={'text-primary'} />}>
+              Lunes a Sábados de 9 a 20 hs
+            </CardListItem>
           </CardList>
 
           <CardSubHeader>Servicios disponibles</CardSubHeader>
@@ -41,6 +52,18 @@ export const Details = () => {
             <Pill>Cargado por Fundación Huesped</Pill>
           </footer>
         </Card>
+
+        <div className={'flex-row'}>
+          <div>
+            <Icon type={'secondary'} circle={true} icon={<GlobeAltIcon className={'text-primary'} />} />
+            <span className={'pt-2 text-center text-xs text-primary'}>{'Sitio Web'}</span>
+          </div>
+          <div>
+            <Icon type={'secondary'} circle={true} icon={<PhoneIcon className={'text-primary'} />} />
+            <span className={'pt-2 text-center text-xs text-primary'}>{'Llamar'}</span>
+          </div>
+        </div>
+
       </MainContainer>
     </>
   );
