deb6a839eddb869b61a7a3fb572dd7cd1fd1334a
Martin Ezequiel Gonzalez <martinegonzalez95@gmail.com>
2022-06-23 01:00:01 -0300

    Chages by linter fix

--- front/src/pages/FAQ.tsx
+++ front/src/pages/FAQ.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { QuestionMarkCircleIcon, ChevronDownIcon } from '@heroicons/react/outline';
 type AccordionItem = {
   id: string;
