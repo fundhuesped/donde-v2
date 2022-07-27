47aa6a4c08899afc41cd068382e6fbc916a85121
Angel Yan <angelyan96@gmail.com>
2022-07-27 19:54:24 -0300

    Move <head> code to custom nextjs document file

---
+++ nextjs/pages/_document.tsx
@@ -0,0 +1,22 @@
+import {Html, Head, Main, NextScript} from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <meta
+          name="description"
+          content="Dónde es una plataforma que te permite encontrar servicios de salud."
+        />
+        <meta name="theme-color" content="#E6334C"/>
+        <link rel="icon" href="/favicon.ico"/>
+        <link rel="apple-touch-icon" href="/logo192.png"/>
+        <link rel="manifest" href="/manifest.json"/>
+      </Head>
+      <body>
+      <Main/>
+      <NextScript/>
+      </body>
+    </Html>
+  )
+}
