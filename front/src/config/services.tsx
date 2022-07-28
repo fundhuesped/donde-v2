cf7cd061ed5879dfb2bf3bc91d43e8d6d69eadbb
Angel Yan <angelyan96@gmail.com>
2022-07-28 10:09:52 -0300

    Delete front/ and back/

--- front/src/config/services.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ReactNode } from 'react';
-import { ReactComponent as TestDeVIH } from '../assets/images/TestDeVIH.svg';
-
-export const SERVICE_ICONS: Record<string, ReactNode> = {
-  'test-hiv': <TestDeVIH />,
-};
--- front/src/utils/establishments.ts
+++
@@ -1,9 +0,0 @@
-function placeInfo(data: string | number | null | undefined) {
-  if (!data) return '';
-  if (typeof data === 'number') return data;
-  return data;
-}
-
-export function formatEstablishmentLocation(place: any) {
-  return `${placeInfo(place.calle)} ${placeInfo(place.altura)}, ${placeInfo(place.nombre_ciudad)}`;
-}
