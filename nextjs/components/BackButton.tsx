a7e5128f09dc5ef79de49b932e142c8c3190a0fd
Angel Yan <angelyan96@gmail.com>
2022-07-27 21:17:59 -0300

    Port header and menu pages

--- nextjs/components/BackButton.tsx
+++ nextjs/components/BackButton.tsx
@@ -1,9 +1,9 @@
-import { useNavigate } from 'react-router-dom';
+import { useRouter } from 'next/router';
 import { ChevronLeftIcon } from '@heroicons/react/solid';
 
 export const BackButton = () => {
-  const navigate = useNavigate();
-  const navigateBack = () => navigate(-1);
+  const router = useRouter()
+  const navigateBack = () => router.back();
   return (
     <button className={'h-6 mr-2'} onClick={navigateBack} aria-label={'Go back'}>
       <ChevronLeftIcon className="w-8 -m-2 text-dark-gray" />
--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // SVG loader
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
