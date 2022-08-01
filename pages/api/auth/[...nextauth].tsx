64cd0cc23c4e17c652097b4dbd3ba6a27bb4c3bb
Angel Yan <angelyan96@gmail.com>
2022-08-01 17:50:16 -0300

    Add user data to JWT token

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -2,6 +2,9 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import {PrismaClient} from "@prisma/client";
 import bcrypt from 'bcrypt';
+import {Session} from "next-auth/core/types";
+import {AuthenticatedUser} from "../../../model/auth";
+import {JWT} from "next-auth/jwt";
 
 const prisma = new PrismaClient()
 
@@ -9,10 +12,10 @@ export default NextAuth({
   providers: [
     CredentialsProvider({
       credentials: {},
-      authorize: async (credentials: any) => {
+      authorize: async (credentials: any): Promise<AuthenticatedUser | null> => {
         const {email, password} = credentials;
         const user = await prisma.user.findUnique({
-          where: { email }
+          where: {email}
         });
 
         if (user && await bcrypt.compare(password, user.password)) {
@@ -27,4 +30,19 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    jwt: async ({token, user}): Promise<JWT> => {
+      if (!user) {
+        return token;
+      }
+      return {...token, user};
+    },
+    session: async ({session, token}): Promise<Session> => {
+      session.user = token['user'] as any;
+      return {
+        ...session,
+        user: token['user'] as any
+      };
+    }
+  }
 });
--- pages/ingresar.tsx
+++ pages/ingresar.tsx
@@ -1,10 +1,11 @@
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { NextPage } from 'next';
+import {signIn, signOut} from 'next-auth/react';
+import {NextPage} from 'next';
 import MainContainer from '../components/MainContainer';
-import { FormEvent, useState } from 'react';
+import {FormEvent, useState} from 'react';
+import {useAuthenticatedUser} from "../hooks/useAuthenticatedUser";
 
 const SignIn: NextPage = () => {
-  const { data: session } = useSession();
+  const user = useAuthenticatedUser();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -22,11 +23,10 @@ const SignIn: NextPage = () => {
       setError('Contraseña incorrecta')
     }
   };
-  console.log(session)
 
   return (
     <MainContainer className={'mt-6 pt-8'}>
-      {session && `Hola ${session.user?.email}`}
+      {user && `Hola ${user.firstName} ${user.lastName}!`}
       <h1 className="text-xl font-bold mb-4">Ingresar</h1>
       <form className={'mt-10'} onSubmit={handleSignIn}>
         <div className="mb-6">
@@ -61,7 +61,7 @@ const SignIn: NextPage = () => {
         >
           Submit
         </button>
-        {session && (
+        {user && (
           <button
             onClick={() => signOut()}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
