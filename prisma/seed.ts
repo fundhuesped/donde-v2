8c74ed91d81e74e00e8ffc8908ab64019f58b2e1
Joaquin Casal <joaquin.casal@10pines.com>
2022-08-25 15:49:18 -0300

    Add initial establishment seed

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import {PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.establishment.create({
+    data: {
+      name: 'Hospital 1',
+      services: {
+        create: [
+          {
+            subtype: 'Subtipo 1',
+            service: {
+              create: {
+                name: 'Servicio 1'
+              }
+            }
+          },
+          {
+            service: {
+              create: {
+                name: 'Servicio 2'
+              }
+            }
+          },
+        ]
+      }
+    }
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
