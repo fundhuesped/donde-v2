cf79ac38bbdd95f7931fb1cfd74cf60fbaf2ec3c
Joaquin Casal <joaquin.casal@10pines.com>
2022-08-30 09:57:57 -0300

    Add create, edit and show endpoints

---
+++ pages/api/establishments/[id].ts
@@ -0,0 +1,59 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiHandler } from 'next';
+import { prismaClient } from '../../../server/prisma/client';
+import { establishmentSchema } from '../../../model/establishment';
+import { EstablishmentStatus } from '@prisma/client';
+import { mapSpecialtiesToPrismaObject } from '../establishments';
+import * as yup from 'yup';
+
+const handler: NextApiHandler = async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      return getEstablishment(req, res);
+    case 'PUT':
+      return updateEstablishment(req, res);
+    default:
+      return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+};
+
+const getEstablishment = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  const idSchema = yup.string().uuid();
+  if (!idSchema.isValidSync(req.query.id)) {
+    return res.status(400).end();
+  }
+  const establishment = await prismaClient.establishment.findUnique({
+    where: {
+      id: req.query.id,
+    },
+  });
+  if (establishment) {
+    return res.status(200).json(establishment);
+  }
+  return res.status(404).end();
+};
+
+const updateEstablishment = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  req.body.id = req.query.id;
+  if (!establishmentSchema.isValidSync(req.body)) {
+    return res.status(400).end();
+  }
+  let data = {};
+  if (req.body.specialties) {
+    const specialties = mapSpecialtiesToPrismaObject(req.body.specialties);
+    data = { specialties: specialties };
+  }
+  const establishment = await prismaClient.establishment.update({
+    where: {
+      id: req.body.id,
+    },
+    data: {
+      ...req.body,
+      status: EstablishmentStatus.PUBLISHED,
+      ...data,
+    },
+  });
+  return res.status(200).json(establishment);
+};
+
+export default handler;
