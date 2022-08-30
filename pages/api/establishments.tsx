6523c199edc524f9e0fbe60cff800ce60cc31e67
Joaquin <joaquin.casal@10pines.com>
2022-08-30 20:21:54 -0300

    Add create, edit and show endpoints (#62)
    * Add create, edit and show endpoints
    
    * Remove unnecessary split
    
    * Separate establishment models
    
    * Solve and remove castings
    
    * Remove unused code

--- pages/api/establishments.tsx
+++ pages/api/establishments.tsx
@@ -1,14 +1,86 @@
-import { NextApiHandler } from 'next';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { prismaClient } from '../../server/prisma/client';
+import { EstablishmentStatus } from '@prisma/client';
+import { createEstablishmentSchema as establishmentSchema } from '../../model/establishment';
+import specialties from './specialties';
+import * as yup from 'yup';
 
 const handler: NextApiHandler = async (req, res) => {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'This endpoint only supports GET requests' });
+  switch (req.method) {
+    case 'GET':
+      return getEstablishments(req, res);
+    case 'POST':
+      return createEstablishment(req, res);
+    default:
+      return res.status(405).json({ error: 'Method Not Allowed' });
   }
+};
 
-  const establishmentsForService = await prismaClient.establishment.findMany();
+const getEstablishments = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  let where = {};
+  const servicesSchema = yup.array().of(yup.string().uuid());
+  const services = req.query.specialties;
+  if (!servicesSchema.isValidSync(services)) {
+    return res.status(400).end();
+  }
+  if (services && services.length !== 0) {
+    where = {
+      specialties: {
+        some: {
+          specialty: {
+            service: {
+              id: { in: services },
+            },
+          },
+        },
+      },
+    };
+  }
+  const establishments = await prismaClient.establishment.findMany({
+    where: where,
+    include: {
+      specialties: {
+        include: {
+          specialty: {
+            include: {
+              service: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  return res.status(200).json(establishments);
+};
 
-  return res.status(200).json(establishmentsForService);
+const createEstablishment = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+  if (!establishmentSchema.isValidSync(req.body)) {
+    return res.status(400).end();
+  }
+  const specialties = mapSpecialtiesToPrismaObject(req.body.specialties);
+  const establishment = await prismaClient.establishment.create({
+    data: {
+      ...req.body,
+      status: EstablishmentStatus.PUBLISHED,
+      specialties: specialties,
+    },
+  });
+  return res.status(201).json(establishment);
+};
+
+export const mapSpecialtiesToPrismaObject = (specialties: string[]) => {
+  const specialtiesObjects = specialties.map((specialtyId: string) => {
+    return {
+      specialty: {
+        connect: {
+          id: specialtyId,
+        },
+      },
+    };
+  });
+  return {
+    create: specialtiesObjects,
+  };
 };
 
 export default handler;
