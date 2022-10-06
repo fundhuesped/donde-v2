7cc3c3073de33012746e473a244b939f547e7062
carabolanteh <carabolanteh@gmail.com>
2022-10-05 21:45:12 -0500

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { useState } from 'react';
-import { Header } from '../components/Header';
-import '@fontsource/poppins';
-import '@fontsource/poppins/700.css';
 import '@fontsource/open-sans';
 import '@fontsource/open-sans/700.css';
-import { SessionProvider } from 'next-auth/react';
-import Script from 'next/script';
+import '@fontsource/poppins';
+import '@fontsource/poppins/700.css';
 import axios from 'axios';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { useState } from 'react';
 import { responseTransformer } from '../axios/transformers';
+import { Header } from '../components/Header';
+import '../styles/globals.css';
 
 axios.defaults.transformResponse = [responseTransformer];
 
@@ -22,6 +22,21 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
   return (
     <SessionProvider session={session}>
       <div className={`min-h-screen flex flex-col ${showScroll}`}>
+         {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script id="my-script" strategy="lazyOnload">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+        </Script>
         <Header onMenuOpening={handleDisableScroll} />
         <Component {...pageProps} />
         <Script src="https://go.botmaker.com/rest/webchat/p/HLENDRUDLS/init.js" />
