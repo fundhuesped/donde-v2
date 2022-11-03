266ad6a6f8fe7f2561eac84de7f2a5d7f9e737d7
Franco Alemandi <francolp59@gmail.com>
2022-11-03 13:20:14 -0300

    hotjar implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,6 +37,24 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
           });
         `}
         </Script>
+        <Script id="my-script-hotjar" strategy="lazyOnload">
+          {`
+          (function (h, o, t, j, a, r) {
+            h.hj =
+              h.hj ||
+              function () {
+                (h.hj.q = h.hj.q || []).push(arguments);
+              };
+            h._hjSettings = { hjid: 3161234, hjsv: 6 };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+        `}
+        
+        </Script>
         <Header onMenuOpening={handleDisableScroll} />
         <Component {...pageProps} />
         <Script src="https://go.botmaker.com/rest/webchat/p/HLENDRUDLS/init.js" />
