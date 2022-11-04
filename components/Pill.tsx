570ccf587b7956a12d4668dd5c1f40c3060d8c65
Franco Alemandi <francolp59@gmail.com>
2022-11-04 19:14:13 -0300

    fix pill onclick prop to be optative

--- components/Pill.tsx
+++ components/Pill.tsx
@@ -5,7 +5,7 @@ type Props = {
   children: string | JSX.Element | JSX.Element[];
   type?: string;
   className: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 export const Pill = React.memo<Props>((props) => {
